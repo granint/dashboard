2d439712b791ae718b769371374e4fae977a75bc
Linkjr <linkjr@arytry.com>
2025-11-09 14:41:11 +0800

    update layout.tsx.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata, ResolvingMetadata } from "next";
+import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { getLocale, getMessages, getTranslations } from "next-intl/server";
